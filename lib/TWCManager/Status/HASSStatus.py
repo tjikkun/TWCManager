0096ffa8d160a676165e22ce8de34ffc68a850db
Nathan Gardiner <ngardiner@gmail.com>
2019-10-06 09:26:55 +1100

    Update HASSStatus.py
    Additional exception handling

--- lib/TWCManager/Status/HASSStatus.py
+++ lib/TWCManager/Status/HASSStatus.py
@@ -41,8 +41,13 @@ class HASSStatus:
         self.debugLog(4, "Error connecting to HomeAssistant to publish sensor values")
         self.debugLog(10, str(e))
         return False
-      except requests.exceptions.ReadTimeout as e:
+      except self.requests.exceptions.ReadTimeout as e:
         self.debugLog(4, "Error connecting to HomeAssistant to publish sensor values")
         self.debugLog(10, str(e))
         return False
+      except Exception as e:
+        self.debugLog(4, "Error during publishing HomeAssistant sensor values")
+        self.debugLog(10, str(e))
+        return False
+        
 
