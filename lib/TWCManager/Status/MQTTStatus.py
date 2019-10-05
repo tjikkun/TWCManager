ddeef7589a1b6a1c18a1fc62f4a68c1d84fd6267
Nathan Gardiner <ngardiner@gmail.com>
2019-10-06 09:12:09 +1100

    Update MQTTStatus.py
    Added debug logging and additional exception handling

--- lib/TWCManager/Status/MQTTStatus.py
+++ lib/TWCManager/Status/MQTTStatus.py
@@ -15,22 +15,30 @@ class MQTTStatus:
     self.status      = status
     self.serverIP    = serverIP
     self.topicPrefix = topicPrefix
-    
+      
+  def debugLog(self, minlevel, message):
+    if (self.debugLevel >= minlevel):
+      print("debugLog: (" + str(minlevel) + ") " + message)
+      
   def setStatus(self, twcid, key, value):
     if (self.status):
       try:
         client = self.mqtt.Client("P1")
         client.connect(self.serverIP)
       except ConnectionRefusedError as e:
-        print("Error connecting to MQTT Broker")
-        print(e)
-        return false
+        self.debugLog(4, "Error connecting to MQTT Broker to publish topic values")
+        self.debugLog(10, str(e))
+        return False
+      except OSError as e:
+        self.debugLog(4, "Error connecting to MQTT Broker to publish topic values")
+        self.debugLog(10, str(e))
+        return False
 
       try:
         client.publish(self.topicPrefix+ "/" + str(twcid.decode("utf-8")) + "/" + key, payload=value)
       except e:
-        print("Error publishing MQTT Topic Status")
-        print(e)
-        return false
+        self.debugLog(4, "Error publishing MQTT Topic Status")
+        self.debugLog(10, str(e))
+        return False
 
       client.disconnect()
